2ef4815f83d1409ab2e87d8f08c41202e17bad7d
Salvador Espinosa <espinosalvador94@outlook.com>
2023-03-07 16:38:06 -0600

    added api key and also made const for days and months

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,3 +5,29 @@ const timezoneEL = document.getElementById("timeZone");
 const countryEL = document.getElementById("country");
 const WeatherForecastEl = document.getElementById("weatherForecastItem");
 const currentTempEL = document.getElementById("currentTemperature");
+
+const day = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+const months = [
+  "Januray",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+const API_KEY = "b2f19c6737c0072132e8bdd78f8b1882";
