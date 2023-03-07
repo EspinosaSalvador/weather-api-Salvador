6afe86862ecb6076adf919990f23e3c06898b04c
Salvador Espinosa <espinosalvador94@outlook.com>
2023-03-07 16:32:37 -0600

    declared variables

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+const timeEl = document.getElementById("time");
+const dateEl = document.getElementById("date");
+const currentWeatherItemEl = document.getElementById("currentWeatherItem");
+const timezoneEL = document.getElementById("timeZone");
+const countryEL = document.getElementById("country");
+const WeatherForecastEl = document.getElementById("weatherForecastItem");
+const currentTempEL = document.getElementById("currentTemperature");
