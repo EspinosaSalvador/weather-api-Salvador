eb7104d2362afacf380264217d76838bf42400ec
Salvador Espinosa <espinosalvador94@outlook.com>
2023-03-08 21:20:30 -0600

    added search history

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,3 +5,20 @@ const API_KEY = "b2f19c6737c0072132e8bdd78f8b1882";
 
 // ! Initialize an empty array to store search history
 let searchHistory = [];
+
+// ! This function handles the search button click event
+function handleSearch() {
+  // ! Get the city name from the input field
+  const city = $("#city-name").val();
+  // ! If the city is not already in the search history, add it
+  if (!searchHistory.includes(city)) {
+    searchHistory.push(city);
+    // ! Add the city name to the search history list in the HTML
+    $("#search-history").append(
+      `<p class="city-history" data-city="${city}">${city}</p>`
+    );
+  }
+  // ! Call the functions to get the current weather and weather forecast for the given city
+  getCurrentWeather(city);
+  getWeatherForecast(city);
+}
