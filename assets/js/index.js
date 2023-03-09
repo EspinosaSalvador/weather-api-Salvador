c4f46ebe3baf148fbfd41cba43084fb0cec72637
Salvador Espinosa <espinosalvador94@outlook.com>
2023-03-08 21:12:34 -0600

    added styles and html but had to redo index.js due that it was not working

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,33 +1,7 @@
-const timeEl = document.getElementById("time");
-const dateEl = document.getElementById("date");
-const currentWeatherItemEl = document.getElementById("currentWeatherItem");
-const timezoneEL = document.getElementById("timeZone");
-const countryEL = document.getElementById("country");
-const WeatherForecastEl = document.getElementById("weatherForecastItem");
-const currentTempEL = document.getElementById("currentTemperature");
-
-const day = [
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-  "Sunday",
-];
-const months = [
-  "Januray",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
-
+// ! Define the API endpoints and API key
+const API_CURRENT = "https://api.openweathermap.org/data/2.5/weather";
+const API_FORECAST = "https://api.openweathermap.org/data/2.5/forecast";
 const API_KEY = "b2f19c6737c0072132e8bdd78f8b1882";
+
+// ! Initialize an empty array to store search history
+let searchHistory = [];
